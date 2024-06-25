486c8e9ab906d288e632bc02854be29cf81f944d
herjara66 <jaramillojesush@gmail.com>
2024-06-25 13:22:46 +0200

    poner colores en CSS y funciones de barajeo de las cartas

--- static/html/app.js
+++ static/html/app.js
@@ -1,6 +1,52 @@
 function flipCard() {
+    if (lockboard) return;
+    if (this === firstCard) return;
+
     this.classList.toggle('flip');
+
+    if (!firstCard) {
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    lockboard = true;
+
+    checkForMatch();
 }
+
+function checkForMatch() {
+    const isMatch = firstCard.dataset.name === secondCard.dataset.name;
+
+    isMatch ? disableCards() : unflipCards();
+    // igualar, desactivar cartas : deshacer cartas
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unfliCards() {
+    setTimeout(() => {
+
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+
+    }, 16);
+
+}
+
+function resetBoard() {
+    [firstCard, secondCard] = [null, null];
+    lockboard = false;
+}
+
+
 
 const cards = document.querySelectorAll('.memory-card');
 cards.forEach(card => card.addEventListener('click', flipCard));
@@ -8,12 +54,12 @@ cards.forEach(card => card.addEventListener('click', flipCard));
 const cardsPareja = [
     { Card: "A", img: "A", class: "purple" },
     { Card: "B", img: "B", class: "amarillo" },
-    { Card: "C", img: "C" },
-    { Card: "D", img: "D" },
+    { Card: "C", img: "C", class: "naranja"},
+    { Card: "D", img: "D", class: "azul"},
     { Card: "A", img: "A", class: "purple" },
     { Card: "B", img: "B", class: "amarillo" },
-    { Card: "C", img: "C" },
-    { Card: "D", img: "D" },
+    { Card: "C", img: "C", class: "naranja" },
+    { Card: "D", img: "D", class: "azul" },
 ];
 
 
